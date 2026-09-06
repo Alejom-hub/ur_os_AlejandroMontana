0ec053f0d1e8da5a6698247a3d927bb85661f5e4
Alejom-hub <alejandrino2006@gmail.com>
2026-09-06 00:10:26 +0000

    adding MFQ and PriorityQueue classes

--- src/ur_os/MFQ.java
+++ src/ur_os/MFQ.java
@@ -1,59 +1,147 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ur_os;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- *
- * @author prestamour
+ * Multilevel Feedback Queue (MFQ)
  */
-public class MFQ extends Scheduler{
+public class MFQ extends Scheduler {
 
     int currentScheduler;
-    
     private ArrayList<Scheduler> schedulers;
-    //This may be a suggestion... you may use the current sschedulers to create the Multilevel Feedback Queue, or you may go with a more tradicional way
-    //based on implementing all the queues in this class... it is your choice. Change all you need in this class.
-    
-    MFQ(OS os){
+
+    public MFQ(OS os) {
         super(os);
         currentScheduler = -1;
-        schedulers = new ArrayList();
+        schedulers = new ArrayList<>();
     }
-    
-    MFQ(OS os, Scheduler... s){ //Received multiple arrays
+
+    public MFQ(OS os, Scheduler... s) {
         this(os);
         schedulers.addAll(Arrays.asList(s));
-        if(s.length > 0)
+        if (s.length > 0) {
             currentScheduler = 0;
+        }
     }
-        
-    @Override
-    public void addProcess(Process p){
-       //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
-        
-    }
-    
-    void defineCurrentScheduler(){
-        //This methos is siggested to help you find the scheduler that should be the next in line to provide processes... perhaps the one with process in the queue?
-    }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        //Suggestion: now that you know on which scheduler a process is, you need to keep advancing that scheduler. If it a preemptive one, you need to notice the changes
-        //that it may have caused and verify if the change is coherent with the priority policy for the queues.
-  
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+    public void addProcess(Process p) {
+        int priority = p.getPriority();
+
+        if (p.getState() == ProcessState.NEW) {
+            // Regla 1: Todo proceso nuevo inicia en la cola de máxima prioridad (Cola 0)
+            priority = 0;
+            p.setPriority(priority);
+            newProcess(os.isCPUEmpty());
+
+        } else if (p.getState() == ProcessState.IO) {
+            // Regla 2: Si regresa de I/O, conserva su prioridad actual (premio a proceso interactivo)
+            IOReturningProcess(os.isCPUEmpty());
+
+        } else if (p.getState() == ProcessState.CPU) {
+            // Regla 3: Si viene de ser expulsado de la CPU por agotar su quantum -> DEGRADACIÓN (Castigo)
+            if (priority < schedulers.size() - 1) {
+                priority++;
+                p.setPriority(priority);
+            }
+            checkPreemption();
+        }
+
+        p.setState(ProcessState.READY);
+
+        // Añadir el proceso a la subcola asignada a su prioridad actual
+        if (priority >= 0 && priority < schedulers.size()) {
+            schedulers.get(priority).addProcess(p);
+        } else {
+            schedulers.get(schedulers.size() - 1).addProcess(p);
+        }
+    }
+
+    private int findHighestPriorityScheduler() {
+        for (int i = 0; i < schedulers.size(); i++) {
+            if (!schedulers.get(i).isEmpty()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void newProcess(boolean cpuEmpty) {
+        checkPreemption();
+    }
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+        checkPreemption();
+    }
+
+    private void checkPreemption() {
+        if (!os.isCPUEmpty()) {
+            Process cpuProcess = os.getProcessInCPU();
+            if (cpuProcess != null) {
+                int currentPriority = cpuProcess.getPriority();
+                int highestPriorityAvailable = findHighestPriorityScheduler();
+
+                // Si un proceso llega a una cola de MAYOR prioridad que el que está ejecutándose en la CPU
+                if (highestPriorityAvailable != -1 && highestPriorityAvailable < currentPriority) {
+                    os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, null);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void getNext(boolean cpuEmpty) {
+        int highestAvailable = findHighestPriorityScheduler();
+
+        if (cpuEmpty) {
+            if (highestAvailable != -1) {
+                currentScheduler = highestAvailable;
+                schedulers.get(currentScheduler).getNext(true);
+            }
+        } else {
+            checkPreemption();
+
+            if (!os.isCPUEmpty()) {
+                Process cpuProcess = os.getProcessInCPU();
+                if (cpuProcess != null) {
+                    int activePriority = cpuProcess.getPriority();
+                    if (activePriority >= 0 && activePriority < schedulers.size()) {
+                        schedulers.get(activePriority).getNext(false);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (Scheduler s : schedulers) {
+            if (!s.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Process removeProcess(Process p) {
+        int priority = p.getPriority();
+        if (priority >= 0 && priority < schedulers.size()) {
+            return schedulers.get(priority).removeProcess(p);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < schedulers.size(); i++) {
+            sb.append("Queue ").append(i).append(":\n");
+            sb.append(schedulers.get(i).toString());
+        }
+        return sb.toString();
+    }
 }
--- src/ur_os/PriorityQueue.java
+++ src/ur_os/PriorityQueue.java
@@ -7,57 +7,133 @@ package ur_os;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-/**
- *
- * @author prestamour
- */
-public class PriorityQueue extends Scheduler{
+public class PriorityQueue extends Scheduler {
 
     int currentScheduler;
-    
     private ArrayList<Scheduler> schedulers;
-    
-    PriorityQueue(OS os){
+
+    public PriorityQueue(OS os) {
         super(os);
         currentScheduler = -1;
-        schedulers = new ArrayList();
+        schedulers = new ArrayList<>();
     }
-    
-    PriorityQueue(OS os, Scheduler... s){ //Received multiple arrays
+
+    public PriorityQueue(OS os, Scheduler... s) {
         this(os);
         schedulers.addAll(Arrays.asList(s));
-        if(s.length > 0)
+        if (s.length > 0) {
             currentScheduler = 0;
+        }
     }
-    
-    
-    @Override
-    public void addProcess(Process p){
-       //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
-       //On which queue should the process go?
-        
-    }
-    
-    void defineCurrentScheduler(){
-        //This methos is suggested to help you find the scheduler that should be the next in line to provide processes... perhaps the one with process in the queue?
-    }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        //Suggestion: now that you know on which scheduler a process is, you need to keep advancing that scheduler. If it a preemptive one, you need to notice the changes
-        //that it may have caused and verify if the change is coherent with the priority policy for the queues.
-        //Suggestion: if the CPU is empty, just find the next scheduler based on the order and the existence of processes
-        //if the CPU is not empty, you need to define that will happen with the process... if it fully preemptive, and there are process pending in higher queue, does the
-        //scheduler removes a process from the CPU or does it let it finish its quantum? Make this decision and justify it.
-  
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+    public void addProcess(Process p) {
+        int priority = p.getPriority();
+
+        // 1. Notificar preempción según el estado previo
+        if (p.getState() == ProcessState.NEW) {
+            newProcess(os.isCPUEmpty());
+        } else if (p.getState() == ProcessState.IO) {
+            IOReturningProcess(os.isCPUEmpty());
+        } else if (p.getState() == ProcessState.CPU) {
+            checkPreemption();
+        }
+
+        // 2. Cambiar estado a READY
+        p.setState(ProcessState.READY);
+
+        // 3. Enviar el proceso a SU SUBCOLA correspondiente (sin meterlo a 'this.processes')
+        if (priority >= 0 && priority < schedulers.size()) {
+            schedulers.get(priority).addProcess(p);
+        } else {
+            schedulers.get(schedulers.size() - 1).addProcess(p);
+        }
+    }
+
+    private int findHighestPriorityScheduler() {
+        for (int i = 0; i < schedulers.size(); i++) {
+            if (!schedulers.get(i).isEmpty()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void newProcess(boolean cpuEmpty) {
+        checkPreemption();
+    }
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+        checkPreemption();
+    }
+
+    private void checkPreemption() {
+        if (!os.isCPUEmpty()) {
+            Process cpuProcess = os.getProcessInCPU();
+            if (cpuProcess != null) {
+                int currentPriority = cpuProcess.getPriority();
+                int highestPriorityAvailable = findHighestPriorityScheduler();
+
+                // Preempción: Si hay alguien en una subcola con MEJOR prioridad (menor índice)
+                if (highestPriorityAvailable != -1 && highestPriorityAvailable < currentPriority) {
+                    os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, null);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void getNext(boolean cpuEmpty) {
+        int highestAvailable = findHighestPriorityScheduler();
+
+        if (cpuEmpty) {
+            if (highestAvailable != -1) {
+                currentScheduler = highestAvailable;
+                schedulers.get(currentScheduler).getNext(true);
+            }
+        } else {
+            checkPreemption();
+
+            if (!os.isCPUEmpty()) {
+                Process cpuProcess = os.getProcessInCPU();
+                if (cpuProcess != null) {
+                    int activePriority = cpuProcess.getPriority();
+                    if (activePriority >= 0 && activePriority < schedulers.size()) {
+                        schedulers.get(activePriority).getNext(false);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (Scheduler s : schedulers) {
+            if (!s.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Process removeProcess(Process p) {
+        int priority = p.getPriority();
+        if (priority >= 0 && priority < schedulers.size()) {
+            return schedulers.get(priority).removeProcess(p);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < schedulers.size(); i++) {
+            sb.append("Queue ").append(i).append(":\n");
+            sb.append(schedulers.get(i).toString());
+        }
+        return sb.toString();
+    }
 }
