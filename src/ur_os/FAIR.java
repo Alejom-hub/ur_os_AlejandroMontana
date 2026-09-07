03426b1deac02bba89ec6a32001434c6c2acf1e1
nataliace21 <natalia20062111@gmail.com>
2026-09-07 02:22:37 +0000

    Creation Fair Algorithm

--- src/ur_os/FAIR.java
+++ src/ur_os/FAIR.java
@@ -1,26 +1,59 @@
 package ur_os;
-import java.util.PriorityQueue;
-import java.util.Comparator;
 
 public class FAIR extends Scheduler {
-    private int someValue;
 
-    public FAIR(OS os, int someValue) {
+    private final int baseQuantum;
+    private int cont;
+
+    public FAIR(OS os) {
+        this(os, 4);
+    }
+
+    public FAIR(OS os, int baseQuantum) {
         super(os);
-        this.someValue = someValue;
+        this.baseQuantum = Math.max(1, baseQuantum);
+        this.cont = 0;
+    }
+
+    private int getQuantum(Process p) {
+        return Math.max(1, baseQuantum - p.getPriority());
     }
 
     @Override
     public void newProcess(boolean cpuEmpty) {
-        
     }
 
     @Override
     public void IOReturningProcess(boolean cpuEmpty) {
-        
     }
+
     @Override
     public void getNext(boolean cpuEmpty) {
-        //Insert code here
+        if (cpuEmpty) {
+            dispatchNext();
+        } else {
+            Process running = os.getProcessInCPU();
+            cont++;
+            if (cont >= getQuantum(running) && !processes.isEmpty()) {
+                os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, null);
+                dispatchNext();
+            }
+        }
+    }
+
+    private void dispatchNext() {
+        if (!processes.isEmpty()) {
+            Process next = processes.poll();
+            cont = 0;
+            addContextSwitch();
+            markFirstExecution(next);
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, next);
+        }
+    }
+
+    private void markFirstExecution(Process process) {
+        if (process.getFirstExecutionTime() == -1) {
+            process.setFirstExecutionTime(os.system.getTime());
+        }
     }
 }
